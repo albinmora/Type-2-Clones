beae0c2bf7f698b374742217385a1ea1b714ced5
Alfredo P <luisalfredo1221@hotmail.com>
2019-06-06 23:09:51 -0600

    Version final de codigo serial y OpenMP

--- CodeClone/main.cpp
+++ CodeClone/main.cpp
@@ -3,16 +3,13 @@
 
 using namespace std;
 
-void ejemplo(){
 
-    // ################################################################################################
-    //                      Proceso de escritura
-    // ################################################################################################
+void generarWorkload(int cantidad, std::string archivo){
 
     ofstream ofs;
 
     // Se abre o crea el archivo
-    ofs.open("/home/alfredo/Documentos/TEC/Arqui_II/Proyecto Final/Proyecto-AST/creado.json");
+    ofs.open(archivo);
 
     // JSON Object
     Json::Value writeValue;
@@ -24,76 +21,51 @@ void ejemplo(){
     Json::StyledWriter sw;
 
     // Se agregan atributos al JSON Object
-    writeValue["language"] = string("Java");
-    writeValue["id"] = 1;
+    writeValue["LN"] = 10;
+    writeValue["IF"] = 1;
+    writeValue["CIC"] = 1;
+    writeValue["RTN"] = 1;
+    writeValue["ARG"] = 2;
+    writeValue["LOC"] = 1;
+    writeValue["FUN"] = 3;
+    writeValue["class"] = "Logger";
+    writeValue["name"] = "foo";
 
-    // Se agrega al JSON Array
-    array.append(writeValue);
+    for(int i = 0; i < cantidad; ++i){
 
-    // Genero otro JSON Object
-    writeValue["language"] = string("Python");
-    writeValue["id"] = 2;
+        // Se agrega al JSON Array
+        array.append(writeValue);
 
-    // Se agrega al JSON Array
-    array.append(writeValue);
+    }
 
     // Escribo el JSON Array al archivo
     ofs << sw.write(array);
 
     // Se cierra el archivo
     ofs.close();
-
-    // ################################################################################################
-    //                      Proceso de lectura
-    // ################################################################################################/
-
-    // Abrir el archivo
-    ifstream ifs("/home/alfredo/Documentos/TEC/Arqui_II/Proyecto Final/Proyecto-AST/creado.json");
-
-    // Objeto para leer el JSON
-    Json::Reader reader;
-
-    // Objeto para guardar un JSON Object leido
-    Json::Value obj;
-
-    // Leer todo el JSON Array o JSON Object que este en el archivo
-    reader.parse(ifs, obj);
-
-    // Obtener el tamano del JSON Array
-    cout << "Size: " << obj.size() << endl;
-
-
-
-    // Obtener el segundo elemento del JSON Array
-    Json::Value method = obj[1];
-
-    // Imprimir los valores para ver que lo lea bien
-    cout << "Language: " << method["language"].asString() << endl;
-    cout << "Id: " << method["id"].asInt() << endl;
-
 }
-
 
 int main(){
 
-    string prueba = "/home/alfredo/Documentos/TEC/Arqui_II/Proyecto Final/Type-2-Clones/prueba.json";
+    //generarWorkload(10000,"/home/alfredo/Documentos/TEC/Arqui_II/Proyecto Final/Type-2-Clones/prueba10k.json");
+
+    string prueba = "/home/alfredo/Documentos/TEC/Arqui_II/Proyecto Final/Type-2-Clones/prueba10k.json";
 
     CloneDetection cd(prueba);
 
     double start_time, run_time;
 
     start_time = omp_get_wtime();
-    vector<int> result = cd.doDetection();
+    //cd.readDetectionResult(cd.doDetection());
+    cd.doDetection();
     run_time = omp_get_wtime() - start_time;
-    //cd.reportCodeClone(result);
 
     cout << "Serial code clone detection in " << run_time << " seconds" << endl << endl;
 
     start_time = omp_get_wtime();
+    //cd.readDetectionResult(cd.doDetection_ThreadVersion());
     cd.doDetection_ThreadVersion();
     run_time = omp_get_wtime() - start_time;
-    //std::cout << "Salio" << std::endl;
-    //cd.reportCodeClone(result);
 
     cout << "OpenMP code clone detection in " << run_time << " seconds" << endl;
 
